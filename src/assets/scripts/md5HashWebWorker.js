68bbdcc9a4934feab9b45e443c0ce7d96e81ea84
Pradeep James <pjm123mail-github@yahoo.co.in>
2017-08-01 20:50:32 -0500

    Added progress status message

--- src/assets/scripts/md5HashWebWorker.js
+++ src/assets/scripts/md5HashWebWorker.js
@@ -9,12 +9,6 @@
 // MD5 checksum library https://github.com/satazor/SparkMD5
 importScripts('spark-md5.js');
 
-// Global variables
-// Note IE 10 does not recognize the const declaration so we have to use var
-var LARGE_FILE = 500 * 1024 * 1024;
-var workerdata = '';
-var asyncstate = true;
-
 // 1MB chunk sizes. The default
 var BYTES_PER_CHUNK = 1 * 1024 * 1024;
 
@@ -31,11 +25,7 @@ function processFile(blob) {
   // Array used to hold the total number of chunks, the number of chunks that have been uploaded,
   // and the current chunk. This information is sent to the web worker that uploads the file chunks
   var chunkCount = {
-
-    currentNumber: 1,
-    numberOfChunks: Total_Number_of_Chunks,
-    numberOfUploadedChunks: 0,
-    starttime: new Date()
+    currentNumber: 1
   };
 
   var start = 0;
@@ -52,27 +42,20 @@ function processFile(blob) {
     var chunk1 = fileReader.readAsArrayBuffer(chunk);
     spark.append(chunk1);
 
-    // Send the chunk back to the parent
-    self.postMessage({ 'type': 'upload', 'filename': blob.name, 'blob': chunk, 'chunkCount':
-    chunkCount, 'asyncstate': asyncstate,'id': workerdata.id });
-
     chunkCount.currentNumber++;
     chunkCount.numberOfUploadedChunks++;
+
+    var percentComplete = parseInt((chunkCount.currentNumber / Total_Number_of_Chunks * 100), 10);
+    self.postMessage({ 'type': 'status', 'message': percentComplete});
 
     start = end;
     end = start + BYTES_PER_CHUNK;
 
-    if(chunkCount.numberOfUploadedChunks == chunkCount.numberOfChunks) {
-
+    if(chunkCount.currentNumber == Total_Number_of_Chunks) {
       // All done calculate the checksum
-      var md5hash = spark.end();
+      var md5hash = spark.end(null);
       console.log('md5hash: ' + md5hash.toUpperCase());
-      self.postMessage({ 'type': 'checksum', 'message': md5hash.toUpperCase(), 'id': workerdata.id
-      });
-
-      // Merge the file on the remote server
-      self.postMessage({ 'type': 'merge', 'filename': blob.name, 'chunkCount': chunkCount, 'id':
-      workerdata.id });
+      self.postMessage({ 'type': 'checksum', 'message': md5hash.toUpperCase() });
     }
   }
 
@@ -82,67 +65,6 @@ function processFile(blob) {
 // This is where we start.
 // The parent sends us the file as a part of the data
 self.onmessage = function (e) {
-
-  workerdata = e.data;
-
-  // If we have an id greater than 5 then we abort. We upload five files at a time.
-  if (workerdata.id > 5) {
-    self.postMessage({ 'type': 'error', 'message': "We can only upload five files at a time.", 'id':
-    workerdata.id });
-    return;
-  }
-
-  // // If we have a large file we will use a synchronous upload by default.
-  // // Large file is greater than 500GB
-  // if(workerdata.files.size > LARGE_FILE && workerdata.uploadlargfileasync == false) {
-  //   asyncstate = false;
-  // }
-
-
-  // Configure the bytes per chunk.
-  // The default is 1MB
-  switch (workerdata.bytesperchunk) {
-
-    case'50MB':
-      BYTES_PER_CHUNK = 50 * 1024 * 1024;
-      break;
-    case'20MB':
-      BYTES_PER_CHUNK = 20 * 1024 * 1024;
-      break;
-    case'10MB':
-      BYTES_PER_CHUNK = 10 * 1024 * 1024;
-      break;
-    case'5MB':
-      BYTES_PER_CHUNK = 5 * 1024 * 1024;
-      break;
-    case'2MB':
-      BYTES_PER_CHUNK = 2 * 1024 * 1024;
-      break;
-    case'1MB':
-      BYTES_PER_CHUNK = 1 * 1024 * 1024;
-      break;
-    case'500K':
-      BYTES_PER_CHUNK = 500 * 1024;
-      break;
-    case'256K':
-      BYTES_PER_CHUNK = 256 * 1024;
-    case'128K':
-      BYTES_PER_CHUNK = 128 * 1024;
-      break;
-    case'64K':
-      BYTES_PER_CHUNK = 64 * 1024;
-      break;
-    default:
-      BYTES_PER_CHUNK = 1024 * 1024;
-  }
-
-
-  // // Process the file for uploading
-  // //  Send a status message to the parent page
-  // self.postMessage({ 'type': 'status', 'message': "Uploading file " + workerdata.files.name, 'id':
-  // workerdata.id });
-
   // Start processing the file
-  processFile(workerdata[0]);
-
+  processFile(e.data[0]);
 };
