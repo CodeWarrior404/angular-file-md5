9f0170998d419d4ec40aeab75158debd04e90cde
Pradeep James <pjm123mail-github@yahoo.co.in>
2017-08-01 21:12:20 -0500

    Refactored md5 Web Worker js

--- src/assets/scripts/md5HashWebWorker.js
+++ src/assets/scripts/md5HashWebWorker.js
@@ -1,70 +1,34 @@
 // https://www.codeproject.com/Articles/830704/Gigabit-File-uploads-Over-HTTP
-
-// This worker is used to split the file into chunks,
-// and calculate the file checksum.
-// Each chunk is sent back to the parent page to be
-// uploaded by another worker
-
-// Import additional scripts
 // MD5 checksum library https://github.com/satazor/SparkMD5
 importScripts('spark-md5.js');
 
-// 1MB chunk sizes. The default
-var BYTES_PER_CHUNK = 1 * 1024 * 1024;
+var BYTES_PER_CHUNK = 1024 * 1024; // 1 MB chunk sizes
 
-// This function is used to read the file, calculate the checksum,
-// and send the file chunk to the web worker that uploads the file chunk
 function processFile(blob) {
-
-  // Size of the file
-  var SIZE = blob.size;
-
-  // The total number of file chunks
-  var Total_Number_of_Chunks = Math.ceil(blob.size / BYTES_PER_CHUNK);
-
-  // Array used to hold the total number of chunks, the number of chunks that have been uploaded,
-  // and the current chunk. This information is sent to the web worker that uploads the file chunks
-  var chunkCount = {
-    currentNumber: 1
-  };
-
-  var start = 0;
-  var end = BYTES_PER_CHUNK;
+  var fileSize = blob.size;
+  var totalNumberOfChunks = Math.ceil(blob.size / BYTES_PER_CHUNK);
+  var processedChunkCounter = 0;
 
   var fileReader = new FileReaderSync();
   var spark = new SparkMD5.ArrayBuffer();
 
-  while (start < SIZE) {
+  var position = 0;
+  while (position < fileSize) {
+    spark.append(fileReader.readAsArrayBuffer(blob.slice(position, position + BYTES_PER_CHUNK)));
+    processedChunkCounter++;
 
-    var chunk = blob.slice(start, end);
+    var percentComplete = parseInt((processedChunkCounter / totalNumberOfChunks * 100), 10);
+    self.postMessage({'type': 'status', 'message': percentComplete});
 
-    // Read the chunk into another variable to calculate the checksum
-    var chunk1 = fileReader.readAsArrayBuffer(chunk);
-    spark.append(chunk1);
-
-    chunkCount.currentNumber++;
-    chunkCount.numberOfUploadedChunks++;
-
-    var percentComplete = parseInt((chunkCount.currentNumber / Total_Number_of_Chunks * 100), 10);
-    self.postMessage({ 'type': 'status', 'message': percentComplete});
-
-    start = end;
-    end = start + BYTES_PER_CHUNK;
-
-    if(chunkCount.currentNumber == Total_Number_of_Chunks) {
-      // All done calculate the checksum
+    if(processedChunkCounter === totalNumberOfChunks) {
       var md5hash = spark.end(null);
+      self.postMessage({'type': 'checksum', 'message': md5hash.toUpperCase()});
       console.log('md5hash: ' + md5hash.toUpperCase());
-      self.postMessage({ 'type': 'checksum', 'message': md5hash.toUpperCase() });
     }
+    position += BYTES_PER_CHUNK;
   }
-
 }
 
-
-// This is where we start.
-// The parent sends us the file as a part of the data
 self.onmessage = function (e) {
-  // Start processing the file
   processFile(e.data[0]);
 };
